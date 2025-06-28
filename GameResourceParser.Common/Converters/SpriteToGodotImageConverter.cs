2664d30d36c4334f48f892887b3c3bb5a44a7e3f
Artem Votintsev <artem.votincev@gmail.com>
2025-06-28 22:36:44 +0200

    Add flip flag for dragon/bee/succub/turtle/bat/etc.

--- GameResourceParser.Common/Converters/SpriteToGodotImageConverter.cs
+++ GameResourceParser.Common/Converters/SpriteToGodotImageConverter.cs
@@ -38,11 +38,14 @@ namespace AllodsParser
                 var newWidth = toConvert.Levels[i].AllSprites.Max(a => a.Width);
                 var newHeight = toConvert.Levels[i].AllSprites.Max(a => a.Height);
 
-                var newImage = new Image<Rgba32>(toConvert.Levels[i].AllSprites.Count * newWidth, newHeight);
+                var countWidth = ((int)Math.Sqrt(toConvert.Levels[i].AllSprites.Count)) + 1;
+                var countHeight = ((int)Math.Sqrt(toConvert.Levels[i].AllSprites.Count)) + 1;
+
+                var newImage = new Image<Rgba32>(countWidth * newWidth, countHeight * newHeight);
 
                 for (int j = 0; j < toConvert.Levels[i].AllSprites.Count; j++)
                 {
-                    newImage.Mutate(a => a.DrawImage(toConvert.Levels[i].AllSprites[j], new Point(newWidth * j, 0), 1));
+                    newImage.Mutate(a => a.DrawImage(toConvert.Levels[i].AllSprites[j], new Point(newWidth * (j % countWidth), newHeight * (j / countWidth)), 1));
                 }
 
                 var filename = toConvert.relativeFileName[0].ToString().ToUpper() + toConvert.relativeFileName.Substring(1) + (i == 0 ? "" : i.ToString());
@@ -80,113 +83,113 @@ public partial class {filename}
 [ext_resource path=""res://Presentation/units/BaseUnit.tscn"" type=""PackedScene"" id=2]
 [ext_resource path=""res://Presentation/units/{filename}/{filename}.cs"" type=""Script"" id=3]
 
-{string.Join("\n", toConvert.Levels[i].AllSprites.Select((a,b) =>
+{string.Join("\n", toConvert.Levels[i].AllSprites.Select((a, b) =>
 
 @$"[sub_resource type=""AtlasTexture"" id={b + 1}]
 flags = 4
 atlas = ExtResource( 1 )
-region = Rect2( {newWidth * b}, 0, {newWidth}, {newHeight} )
+region = Rect2( {newWidth * (b % countWidth)}, {newHeight * (b / countWidth)}, {a.Width}, {a.Height} )
 "
 ))}
 
 [sub_resource type=""SpriteFrames"" id={toConvert.Levels[i].AllSprites.Count + 1}]
 animations = [ 
-{{""frames"": [ { string.Join(", ", toConvert.Levels[i].AttackBottom.Select(f => $"SubResource({toConvert.Levels[i].AllSprites.IndexOf(f)})")) } ],
+{{""frames"": [ {string.Join(", ", toConvert.Levels[i].AttackBottom.Select(f => $"SubResource({toConvert.Levels[i].AllSprites.IndexOf(f) + 1})"))} ],
 ""loop"": true,
 ""name"": ""AttackBottom"",
-""speed"": 5.0
-}}, {{""frames"": [ { string.Join(", ", toConvert.Levels[i].AttackBottomLeft.Select(f => $"SubResource({toConvert.Levels[i].AllSprites.IndexOf(f)})")) } ],
+""speed"": {toConvert.Levels[i].AttackBottom.Count() / toConvert.Levels[i].AttackLength}
+}}, {{""frames"": [ {string.Join(", ", toConvert.Levels[i].AttackBottomLeft.Select(f => $"SubResource({toConvert.Levels[i].AllSprites.IndexOf(f) + 1})"))} ],
 ""loop"": true,
 ""name"": ""AttackBottomLeft"",
-""speed"": 5.0
-}}, {{""frames"": [ { string.Join(", ", toConvert.Levels[i].AttackBottomRight.Select(f => $"SubResource({toConvert.Levels[i].AllSprites.IndexOf(f)})")) } ],
+""speed"": {toConvert.Levels[i].AttackBottomLeft.Count() / toConvert.Levels[i].AttackLength}
+}}, {{""frames"": [ {string.Join(", ", toConvert.Levels[i].AttackBottomRight.Select(f => $"SubResource({toConvert.Levels[i].AllSprites.IndexOf(f) + 1})"))} ],
 ""loop"": true,
 ""name"": ""AttackBottomRight"",
-""speed"": 5.0
-}}, {{""frames"": [ { string.Join(", ", toConvert.Levels[i].AttackLeft.Select(f => $"SubResource({toConvert.Levels[i].AllSprites.IndexOf(f)})")) } ],
+""speed"": {toConvert.Levels[i].AttackBottomRight.Count() / toConvert.Levels[i].AttackLength}
+}}, {{""frames"": [ {string.Join(", ", toConvert.Levels[i].AttackLeft.Select(f => $"SubResource({toConvert.Levels[i].AllSprites.IndexOf(f) + 1})"))} ],
 ""loop"": true,
 ""name"": ""AttackLeft"",
-""speed"": 5.0
-}}, {{""frames"": [ { string.Join(", ", toConvert.Levels[i].AttackRight.Select(f => $"SubResource({toConvert.Levels[i].AllSprites.IndexOf(f)})")) } ],
+""speed"": {toConvert.Levels[i].AttackLeft.Count() / toConvert.Levels[i].AttackLength}
+}}, {{""frames"": [ {string.Join(", ", toConvert.Levels[i].AttackRight.Select(f => $"SubResource({toConvert.Levels[i].AllSprites.IndexOf(f) + 1})"))} ],
 ""loop"": true,
 ""name"": ""AttackRight"",
-""speed"": 5.0
-}}, {{""frames"": [ { string.Join(", ", toConvert.Levels[i].AttackTop.Select(f => $"SubResource({toConvert.Levels[i].AllSprites.IndexOf(f)})")) } ],
+""speed"": {toConvert.Levels[i].AttackRight.Count() / toConvert.Levels[i].AttackLength}
+}}, {{""frames"": [ {string.Join(", ", toConvert.Levels[i].AttackTop.Select(f => $"SubResource({toConvert.Levels[i].AllSprites.IndexOf(f) + 1})"))} ],
 ""loop"": true,
 ""name"": ""AttackTop"",
-""speed"": 5.0
-}}, {{""frames"": [ { string.Join(", ", toConvert.Levels[i].AttackTopLeft.Select(f => $"SubResource({toConvert.Levels[i].AllSprites.IndexOf(f)})")) } ],
+""speed"": {toConvert.Levels[i].AttackTop.Count() / toConvert.Levels[i].AttackLength}
+}}, {{""frames"": [ {string.Join(", ", toConvert.Levels[i].AttackTopLeft.Select(f => $"SubResource({toConvert.Levels[i].AllSprites.IndexOf(f) + 1})"))} ],
 ""loop"": true,
 ""name"": ""AttackTopLeft"",
-""speed"": 5.0
-}}, {{""frames"": [ { string.Join(", ", toConvert.Levels[i].AttackTopRight.Select(f => $"SubResource({toConvert.Levels[i].AllSprites.IndexOf(f)})")) } ],
+""speed"": {toConvert.Levels[i].AttackTopLeft.Count() / toConvert.Levels[i].AttackLength}
+}}, {{""frames"": [ {string.Join(", ", toConvert.Levels[i].AttackTopRight.Select(f => $"SubResource({toConvert.Levels[i].AllSprites.IndexOf(f) + 1})"))} ],
 ""loop"": true,
 ""name"": ""AttackTopRight"",
-""speed"": 5.0
-}}, {{""frames"": [ { string.Join(", ", toConvert.Levels[i].MoveBottom.Select(f => $"SubResource({toConvert.Levels[i].AllSprites.IndexOf(f)})")) } ],
+""speed"": {toConvert.Levels[i].AttackTopRight.Count() / toConvert.Levels[i].AttackLength}
+}}, {{""frames"": [ {string.Join(", ", toConvert.Levels[i].MoveBottom.Select(f => $"SubResource({toConvert.Levels[i].AllSprites.IndexOf(f) + 1})"))} ],
 ""loop"": true,
 ""name"": ""MoveBottom"",
-""speed"": 5.0
-}}, {{""frames"": [ { string.Join(", ", toConvert.Levels[i].MoveBottomLeft.Select(f => $"SubResource({toConvert.Levels[i].AllSprites.IndexOf(f)})")) } ],
+""speed"": {toConvert.Levels[i].MoveBottom.Count() / toConvert.Levels[i].MoveLength}
+}}, {{""frames"": [ {string.Join(", ", toConvert.Levels[i].MoveBottomLeft.Select(f => $"SubResource({toConvert.Levels[i].AllSprites.IndexOf(f) + 1})"))} ],
 ""loop"": true,
 ""name"": ""MoveBottomLeft"",
-""speed"": 5.0
-}}, {{""frames"": [ { string.Join(", ", toConvert.Levels[i].MoveBottomRight.Select(f => $"SubResource({toConvert.Levels[i].AllSprites.IndexOf(f)})")) } ],
+""speed"": {toConvert.Levels[i].MoveBottomLeft.Count() / toConvert.Levels[i].MoveLength}
+}}, {{""frames"": [ {string.Join(", ", toConvert.Levels[i].MoveBottomRight.Select(f => $"SubResource({toConvert.Levels[i].AllSprites.IndexOf(f) + 1})"))} ],
 ""loop"": true,
 ""name"": ""MoveBottomRight"",
-""speed"": 5.0
-}}, {{""frames"": [ { string.Join(", ", toConvert.Levels[i].MoveLeft.Select(f => $"SubResource({toConvert.Levels[i].AllSprites.IndexOf(f)})")) } ],
+""speed"": {toConvert.Levels[i].MoveBottomRight.Count() / toConvert.Levels[i].MoveLength}
+}}, {{""frames"": [ {string.Join(", ", toConvert.Levels[i].MoveLeft.Select(f => $"SubResource({toConvert.Levels[i].AllSprites.IndexOf(f) + 1})"))} ],
 ""loop"": true,
 ""name"": ""MoveLeft"",
-""speed"": 5.0
-}}, {{""frames"": [ { string.Join(", ", toConvert.Levels[i].MoveRight.Select(f => $"SubResource({toConvert.Levels[i].AllSprites.IndexOf(f)})")) } ],
+""speed"": {toConvert.Levels[i].MoveLeft.Count() / toConvert.Levels[i].MoveLength}
+}}, {{""frames"": [ {string.Join(", ", toConvert.Levels[i].MoveRight.Select(f => $"SubResource({toConvert.Levels[i].AllSprites.IndexOf(f) + 1})"))} ],
 ""loop"": true,
 ""name"": ""MoveRight"",
-""speed"": 5.0
-}}, {{""frames"": [ { string.Join(", ", toConvert.Levels[i].MoveTop.Select(f => $"SubResource({toConvert.Levels[i].AllSprites.IndexOf(f)})")) } ],
+""speed"": {toConvert.Levels[i].MoveRight.Count() / toConvert.Levels[i].MoveLength}
+}}, {{""frames"": [ {string.Join(", ", toConvert.Levels[i].MoveTop.Select(f => $"SubResource({toConvert.Levels[i].AllSprites.IndexOf(f) + 1})"))} ],
 ""loop"": true,
 ""name"": ""MoveTop"",
-""speed"": 5.0
-}}, {{""frames"": [ { string.Join(", ", toConvert.Levels[i].MoveTopLeft.Select(f => $"SubResource({toConvert.Levels[i].AllSprites.IndexOf(f)})")) } ],
+""speed"": {toConvert.Levels[i].MoveTop.Count() / toConvert.Levels[i].MoveLength}
+}}, {{""frames"": [ {string.Join(", ", toConvert.Levels[i].MoveTopLeft.Select(f => $"SubResource({toConvert.Levels[i].AllSprites.IndexOf(f) + 1})"))} ],
 ""loop"": true,
 ""name"": ""MoveTopLeft"",
-""speed"": 5.0
-}}, {{""frames"": [ { string.Join(", ", toConvert.Levels[i].MoveTopRight.Select(f => $"SubResource({toConvert.Levels[i].AllSprites.IndexOf(f)})")) } ],
+""speed"": {toConvert.Levels[i].MoveTopLeft.Count() / toConvert.Levels[i].MoveLength}
+}}, {{""frames"": [ {string.Join(", ", toConvert.Levels[i].MoveTopRight.Select(f => $"SubResource({toConvert.Levels[i].AllSprites.IndexOf(f) + 1})"))} ],
 ""loop"": true,
 ""name"": ""MoveTopRight"",
-""speed"": 5.0
-}}, {{""frames"": [ { string.Join(", ", toConvert.Levels[i].StayBottom.Select(f => $"SubResource({toConvert.Levels[i].AllSprites.IndexOf(f)})")) } ],
+""speed"": {toConvert.Levels[i].MoveTopRight.Count() / toConvert.Levels[i].MoveLength}
+}}, {{""frames"": [ {string.Join(", ", toConvert.Levels[i].StayBottom.Select(f => $"SubResource({toConvert.Levels[i].AllSprites.IndexOf(f) + 1})"))} ],
 ""loop"": true,
 ""name"": ""StayBottom"",
-""speed"": 5.0
-}}, {{""frames"": [ { string.Join(", ", toConvert.Levels[i].StayBottomLeft.Select(f => $"SubResource({toConvert.Levels[i].AllSprites.IndexOf(f)})")) } ],
+""speed"": {toConvert.Levels[i].StayBottom.Count() / toConvert.Levels[i].StayLength}
+}}, {{""frames"": [ {string.Join(", ", toConvert.Levels[i].StayBottomLeft.Select(f => $"SubResource({toConvert.Levels[i].AllSprites.IndexOf(f) + 1})"))} ],
 ""loop"": true,
 ""name"": ""StayBottomLeft"",
-""speed"": 5.0
-}}, {{""frames"": [ { string.Join(", ", toConvert.Levels[i].StayBottomRight.Select(f => $"SubResource({toConvert.Levels[i].AllSprites.IndexOf(f)})")) } ],
+""speed"": {toConvert.Levels[i].StayBottomLeft.Count() / toConvert.Levels[i].StayLength}
+}}, {{""frames"": [ {string.Join(", ", toConvert.Levels[i].StayBottomRight.Select(f => $"SubResource({toConvert.Levels[i].AllSprites.IndexOf(f) + 1})"))} ],
 ""loop"": true,
 ""name"": ""StayBottomRight"",
-""speed"": 5.0
-}}, {{""frames"": [ { string.Join(", ", toConvert.Levels[i].StayLeft.Select(f => $"SubResource({toConvert.Levels[i].AllSprites.IndexOf(f)})")) } ],
+""speed"": {toConvert.Levels[i].StayBottomRight.Count() / toConvert.Levels[i].StayLength}
+}}, {{""frames"": [ {string.Join(", ", toConvert.Levels[i].StayLeft.Select(f => $"SubResource({toConvert.Levels[i].AllSprites.IndexOf(f) + 1})"))} ],
 ""loop"": true,
 ""name"": ""StayLeft"",
-""speed"": 5.0
-}}, {{""frames"": [ { string.Join(", ", toConvert.Levels[i].StayRight.Select(f => $"SubResource({toConvert.Levels[i].AllSprites.IndexOf(f)})")) } ],
+""speed"": {toConvert.Levels[i].StayLeft.Count() / toConvert.Levels[i].StayLength}
+}}, {{""frames"": [ {string.Join(", ", toConvert.Levels[i].StayRight.Select(f => $"SubResource({toConvert.Levels[i].AllSprites.IndexOf(f) + 1})"))} ],
 ""loop"": true,
 ""name"": ""StayRight"",
-""speed"": 5.0
-}}, {{""frames"": [ { string.Join(", ", toConvert.Levels[i].StayTop.Select(f => $"SubResource({toConvert.Levels[i].AllSprites.IndexOf(f)})")) } ],
+""speed"": {toConvert.Levels[i].StayRight.Count() / toConvert.Levels[i].StayLength}
+}}, {{""frames"": [ {string.Join(", ", toConvert.Levels[i].StayTop.Select(f => $"SubResource({toConvert.Levels[i].AllSprites.IndexOf(f) + 1})"))} ],
 ""loop"": true,
 ""name"": ""StayTop"",
-""speed"": 5.0
-}}, {{""frames"": [ { string.Join(", ", toConvert.Levels[i].StayTopLeft.Select(f => $"SubResource({toConvert.Levels[i].AllSprites.IndexOf(f)})")) } ],
+""speed"": {toConvert.Levels[i].StayTop.Count() / toConvert.Levels[i].StayLength}
+}}, {{""frames"": [ {string.Join(", ", toConvert.Levels[i].StayTopLeft.Select(f => $"SubResource({toConvert.Levels[i].AllSprites.IndexOf(f) + 1})"))} ],
 ""loop"": true,
 ""name"": ""StayTopLeft"",
-""speed"": 5.0
-}}, {{""frames"": [ { string.Join(", ", toConvert.Levels[i].StayTopRight.Select(f => $"SubResource({toConvert.Levels[i].AllSprites.IndexOf(f)})")) } ],
+""speed"": {toConvert.Levels[i].StayTopLeft.Count() / toConvert.Levels[i].StayLength}
+}}, {{""frames"": [ {string.Join(", ", toConvert.Levels[i].StayTopRight.Select(f => $"SubResource({toConvert.Levels[i].AllSprites.IndexOf(f) + 1})"))} ],
 ""loop"": true,
 ""name"": ""StayTopRight"",
-""speed"": 5.0
+""speed"": {toConvert.Levels[i].StayTopRight.Count() / toConvert.Levels[i].StayLength}
 }} ]
 
 [sub_resource type=""AnimationNodeStateMachinePlayback"" id={toConvert.Levels[i].AllSprites.Count + 2}]
@@ -197,25 +200,26 @@ MaxHP = 30
 HP = 30
 AttackPower = 5
 AttackDistance = 100.0
-AttackDelay = 1.0
+AttackDelay = {toConvert.Levels[i].AttackLength}
+HitDelay = {toConvert.Levels[i].HitDelay}
 MoveSpeed = 100.0
 MoveFloors = PoolIntArray( 2 )
-Loot = [ ExtResource( 6 ) ]
 
 [node name=""Healthbar"" parent=""."" index=""0""]
 position = Vector2( 28, -41 )
 
 [node name=""AnimatedSprite"" parent=""."" index=""1""]
 position = Vector2( 0, -49 )
-frames = SubResource( 97 )
+frames = SubResource( {toConvert.Levels[i].AllSprites.Count + 1} )
 animation = ""StayBottom""
+centered = true
 
 [node name=""AnimationTree"" parent=""."" index=""4""]
-parameters/playback = SubResource( 146 )
+parameters/playback = SubResource( {toConvert.Levels[i].AllSprites.Count + 2} )
 
 [editable path=""QuestPopup""]
-[editable path=""SignPopup""]                    
-                    "
+[editable path=""SignPopup""]
+"
                 };
             }
         }
